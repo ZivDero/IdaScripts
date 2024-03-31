730b2668576799783ed7d6b0e3cfe0a1b68845cb
ZivDero <kirill.andriiashin@gmail.com>
2024-03-31 15:59:13 +0300

    v2

--- PrepareDisasm/main.py
+++ PrepareDisasm/main.py
@@ -10,27 +10,32 @@ JUMP_OPERATORS = ['JMP', 'JO', 'JNO', 'JS', 'JNS', 'JE', 'JZ', 'JNE', 'JNZ', 'JB
 
 out_path = "D:\\Projects\\TSRe\\diff\\"
 file_name = "Draw_Shape"
-assembler_path = "D:\\Projects\\JWasm211bw\\JWASM.EXE"
+assembler_path = "G:\\masm32\\bin\\ml.exe"
 
 asm_file_path = out_path + file_name + ".asm"
 object_file_path = out_path + file_name + ".o"
 
 class Instruction:
-    def __init__(self, ip: str, disasm: str, has_label=False):
+    def __init__(self, ip: int, opcode: str, operands: list, has_label=False):
         self.ip = ip
-        self.disasm = disasm
+        self.opcode = opcode
+        self.operands = operands
         self.has_label = has_label
 
     def __str__(self):
         string = ""
         if self.has_label:
             string += f"\nlabel_{self.ip:X}:\n"
-        string += self.disasm
+        string += f"{self.opcode} {','.join(self.operands)}"
         return string
 
 
-def hex_to_int(string):
+def hex_0x_to_int(string):
     return int(f"0x{string.replace('h', '')}", 16)
+
+
+def int_to_hex(number):
+    return f"{number:X}"
 
 
 def sanitize_string(string):
@@ -41,6 +46,8 @@ def sanitize_string(string):
     return new_string
 
 
+address_regex = re.compile("[A-Z0-9]+h(?=[^A-Z0-9]*?)")
+
 function = ida_funcs.get_func(here())
 function_start = function.start_ea
 function_bytes = ida_bytes.get_bytes(function.start_ea, function.end_ea - function.start_ea)
@@ -49,78 +56,88 @@ decoder = Decoder(32, function_bytes, ip=function_start)
 formatter = Formatter(FormatterSyntax.MASM)
 
 instructions = list()
-
-address_regex = re.compile("[a-zA-Z0-9]+h$")
-address_regex_soft = re.compile("[a-zA-Z0-9]+h(?=[^a-zA-Z0-9]*?)")
-
 for instr in decoder:
     disasm = formatter.format(instr)
     start_index = instr.ip - function_start
     bytes_str = function_bytes[start_index:start_index + instr.len].hex().upper()
-    instructions.append(Instruction(instr.ip, disasm))
+
+    parts = disasm.split(" ")
+    opcode = parts[0]
+    operands = " ".join(parts[1:]).split(",") if len(parts) > 1 else []
+
+    instructions.append(Instruction(instr.ip, opcode, operands))
 
 
 for instr in instructions:
-    name, *operands = re.split("[ ,]", instr.disasm)
-    if name.upper() in JUMP_OPERATORS:
-        address, *_ = [*filter(address_regex.match, operands), None]
-        address_int = hex_to_int(address)
-        instr.disasm = instr.disasm.replace(address, f"label_{address_int:X}")
+    if instr.opcode.upper() in JUMP_OPERATORS:
+        try:
+            jump_address_match = address_regex.search(instr.operands[0])
 
-        for instr2 in instructions:
-            if address_int == instr2.ip:
-                instr2.has_label = True
-                break
-        else:
-            print(f"WARNING: JUMP ADDRESS NOT FOUND {address}")
-            exit()
+            if jump_address_match is None:
+                continue
+
+            jump_address = jump_address_match.group(0)
+            jump_address_int = hex_0x_to_int(jump_address)
+            instr.operands[0] = instr.operands[0].replace(jump_address, f"label_{jump_address_int:X}")
+
+            for dest_instr in instructions:
+                if jump_address_int == dest_instr.ip:
+                    dest_instr.has_label = True
+                    break
+
+        except:
+            print(f"WARNING: Jump address not found. IP: {instr.ip:X}. {instr.opcode} {instr.operands}")
+            continue
+
 
 calls = list()
 for instr in instructions:
-    name, *operands = re.split("[ ,]", instr.disasm)
-    if name.upper() == "CALL":
-        address, *_ = [*filter(address_regex.match, operands), None]
-        if address is not None:
-            instr.disasm = instr.disasm.replace(address, f"func_{hex_to_int(address):X}")
-            calls.append(f"func_{hex_to_int(address):X}")
+    if instr.opcode.upper() == "CALL":
+        try:
+            call_address_match = address_regex.search(instr.operands[0])
 
-globals = list()
+            if call_address_match is None:
+                continue
+
+            call_address = call_address_match.group(0)
+            call_address_int = hex_0x_to_int(call_address)
+            func_name = f"func_{call_address_int:X}"
+            instr.operands[0] = instr.operands[0].replace(call_address, func_name)
+
+            if func_name not in calls:
+                calls.append(func_name)
+
+        except:
+            print(f"WARNING: Call address not found. IP: {instr.ip:X}. {instr.opcode} {instr.operands}")
+            continue
+
+
+global_vars = list()
 for instr in instructions:
-    parts = instr.disasm.split(" ")
-    name = parts[0]
-    operands = " ".join(parts[1:]).split(",") if len(parts) > 1 else []
-
-    if name.upper() in JUMP_OPERATORS or name.upper() == "CALL":
+    if instr.opcode.upper() in JUMP_OPERATORS or instr.opcode.upper() == "CALL":
         continue
 
-    raw_addresses = list(set([match.group(0) for match in map(address_regex_soft.search, operands) if match is not None]))
-    address_numbers = list((map(hex_to_int, raw_addresses)))
-    address_names = dict()
-
-    new_operands = list()
-    for address, string in zip(address_numbers, raw_addresses):
-        if address < 0x401000:
+    for i in range(len(instr.operands)):
+        operand = instr.operands[i]
+        address_match = address_regex.search(operand)
+        if address_match is None:
             continue
-        global_name = ida_name.get_nice_colored_name(address, 3)
+
+        address_str = address_match.group(0)
+        address_int = hex_0x_to_int(address_str)
+        if address_int < 0x401000:
+            continue
+
+        global_name = ida_name.get_nice_colored_name(address_int, 3)
         global_name_stripped = sanitize_string(global_name if '+' not in global_name else global_name.split('+')[0])
-        if global_name_stripped not in globals:
-            globals.append(global_name_stripped)
+        if global_name_stripped not in global_vars:
+            global_vars.append(global_name_stripped)
 
-        address_names[string] = global_name_stripped
-
-    for operand in operands:
-        for op_name, new_name in address_names.items():
-            if op_name in operand:
-                if len(op_name) == len(operand):
-                    new_operands.append(f"offset {new_name}")
-                else:
-                    new_operands.append(operand.replace(op_name, new_name))
-                break
+        if len(address_str) == len(operand):
+            instr.operands[i] = f"offset {global_name_stripped}"
         else:
-            new_operands.append(operand)
+            instr.operands[i] = operand.replace(address_str, global_name_stripped)
 
-    new_disasm = name + " " + ",".join(new_operands)
-    instr.disasm = new_disasm
 
 with open(asm_file_path, "w") as f:
     f.write(".486\n.model flat\n.code _DIFF_SEG\n\n")
@@ -130,8 +147,8 @@ with open(asm_file_path, "w") as f:
 
     f.write("\n")
 
-    for global_ in globals:
-        f.write(f"externdef {global_}:dword\n")
+    for global_var in global_vars:
+        f.write(f"externdef {global_var}:dword\n")
 
     f.write("\n")
 
@@ -140,4 +157,4 @@ with open(asm_file_path, "w") as f:
 
     f.write("\nEND\n")
 
-subprocess.Popen([assembler_path, f" \"asm_file_path\" /coff -Fo \"{object_file_path}\""])
+subprocess.call(f"{assembler_path} /c /coff /Fo\"{object_file_path}\" \"{asm_file_path}\"", shell=False)
